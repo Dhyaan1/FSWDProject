3768c79c074989816307f2af03bae605a1c24960
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-06-09 21:10:16 +0530

    feat: Update page layout and metadata
    + Update the page layout and metadata to reflect the new branding and messaging for Azayd IT Consulting Services. This includes updating the page title and description to better align with the company's services and value proposition. Additionally, modify the body class to include a new CSS class for improved styling.
    
    Co-authored-by: Dharmisht <120662194+dragn0id@users.noreply.github.com>

--- fswd/app/layout.tsx
+++ fswd/app/layout.tsx
@@ -1,4 +1,3 @@
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Archivo } from "next/font/google";
 
@@ -14,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Azayd IT Consulting Services",
+  description: "Elevate Your IT with Our Consultancy Services",
 };
 
 export default function RootLayout({
@@ -25,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={archivo.variable}>{children}</body>
+      <body className={`${archivo.variable} relative`}>{children}</body>
     </html>
   );
 }
