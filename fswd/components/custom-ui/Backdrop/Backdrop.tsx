cab34177bc85cdd62360ce9d4f7470f725f87011
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-06-10 16:06:10 +0530

    chore: Update Backdrop component with responsive width and height values
    + Update the Backdrop component to use responsive width and height values (`w-dvw` and `h-dvh`) instead of fixed values. This ensures that the component adapts to different screen sizes and provides a better user experience.

--- fswd/components/custom-ui/Backdrop/Backdrop.tsx
+++ fswd/components/custom-ui/Backdrop/Backdrop.tsx
@@ -4,7 +4,7 @@ const Backdrop = ({ children, onClick }) => {
   return (
     <motion.div
       onClick={onClick}
-      className="flex justify-center items-center fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50"
+      className="flex justify-center items-center fixed top-0 left-0 w-dvw h-dvh bg-black bg-opacity-50 z-50"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.3 } }}
       exit={{ opacity: 0, transition: { duration: 0.3 } }}
